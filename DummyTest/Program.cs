9cce985c96f86716c8246fa4b3f730dad90a775b
Jakub Puacz <s30970@pjwstk.edu.pl>
2025-03-03 15:09:59 +0100

    average computations

--- DummyTest/Program.cs
+++ DummyTest/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Goodbye world!");
+
+
+static double GetAverage(int[] numbers)
+{
+    return Enumerable.Average(numbers);
+}
+
+
+int[] array = { 1, 2, 3, 4, 5 };
+GetAverage(array);
